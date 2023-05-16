0b24f72b4104e710399962f13fda885f022bec22
sarobinson2011 <sarobinson2011@gmail.com>
2023-05-16 16:24:58 +0100

    add up until VRF section

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -2,29 +2,52 @@
 pragma solidity ^0.6.6;
 
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 
-contract Lottery {
+contract Lottery is Ownable {
+    using SafeMath for uint256;
+
     address payable[] public players;
     uint256 public usdEntryFee;
     AggregatorV3Interface internal ethUsdPriceFeed;
 
+    // OPEN = 0, CLOSED = 1, CALCULATING_WINNER = 2
+    enum LOTTERY_STATE {
+        OPEN,
+        CLOSED,
+        CALCULATING_WINNER
+    }
+
+    LOTTERY_STATE public lottery_state;
+
     constructor(address _priceFeedAddress) public {
         usdEntryFee = 50 * (10 ** 18);
         ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
+        lottery_state = LOTTERY_STATE.CLOSED;
     }
 
     function enter() public payable {
-        // $50 minimum
+        // Need $50 minimum && Lottery = OPEN
+        require(lottery_state == LOTTERY_STATE.OPEN);
+        require(msg.value >= getEntranceFee(), "Not enough ETH!");
         players.push(msg.sender);
     }
 
-    function getEntranceFee() public {
-        // use chainlink price feed for ETH/USD
+    function getEntranceFee() public view returns (uint256) {
+        (, int256 price, , , ) = ethUsdPriceFeed.latestRoundData();
+        uint256 adjustedPrice = uint256(price) * 10 ** 10;
+        uint256 costToEnter = (usdEntryFee * 10 ** 18) / adjustedPrice;
+        return costToEnter;
     }
 
-    function startLottery() public {}
+    function startLottery() public onlyOwner {
+        require(
+            lottery_state == LOTTERY_STATE.CLOSED,
+            "Can't start a new lottery yet!"
+        );
+        lottery_state = LOTTERY_STATE.OPEN;
+    }
 
-    function endLottery() public {}
+    function endLottery() public onlyOwner {}
 }
-
-// paused at --> 6:19:13 / 16:22:11
